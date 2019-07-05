10472fdb949b5dcf77083e11d3a1e752ad41dee0
Rafael Neto <rafael.trilha@gmail.com>
2019-07-05 02:21:42 -0300

    deletado arquivo retangule e alterado o arquivo program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,11 @@
 package application;
 
-import java.util.Locale;
-import java.util.Scanner;
-
-import entities.Retangule;
-
 public class Program {
 
 	public static void main(String[] args) {
-
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-
-		Retangule rectangle = new Retangule();
 		
-		System.out.print("Enter rectangle width and height: ");
-		rectangle.width = sc.nextDouble();
-		rectangle.height = sc.nextDouble();
-	
-		System.out.printf("Area = %.2f%n", rectangle.area());
- 		System.out.printf("Perimeter = %.2f%n", rectangle.perimeter());
-		System.out.printf("Diagonal = %.2f%n", rectangle.diagonal());
-		
-		sc.close();
+		System.out.println("Bom dia!");
+
 	}
 
 }
